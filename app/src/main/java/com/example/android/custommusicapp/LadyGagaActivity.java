8f23b37257fad2a2df60ba6f1edcf34523de0348
jamesyothers <jamesjsdev@gmail.com>
2019-09-02 16:57:03 -0500

    feat: update Lady Gaga songs

--- app/src/main/java/com/example/android/custommusicapp/LadyGagaActivity.java
+++ app/src/main/java/com/example/android/custommusicapp/LadyGagaActivity.java
@@ -17,11 +17,11 @@ public class LadyGagaActivity extends AppCompatActivity {
 
         ArrayList<Song> words = new ArrayList<Song>();
 
-        words.add(new Song("one", "lutti", "yes"));
-        words.add(new Song("one", "lutti", "yes"));
-        words.add(new Song("one", "lutti", "yes"));
-        words.add(new Song("one", "lutti", "yes"));
-        words.add(new Song("one", "lutti", "yes"));
+        words.add(new Song("Song: Applause", "Artist: Lady Gaga", "Album: ARTPOP"));
+        words.add(new Song("Song: Paparazzi", "Artist: Lady Gaga", "Album: The Fame"));
+        words.add(new Song("Song: Speechless", "Artist: Lady Gaga", "Album: The Fame Monster"));
+        words.add(new Song("Song: Angel Down", "Artist: Lady Gaga", "Album: Joanne"));
+        words.add(new Song("Song: Bad Kids", "Artist: Lady Gaga", "Album: Borne This Way"));
 
         SongAdapter songAdapter = new SongAdapter(this, words);
 
